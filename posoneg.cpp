fee5a0d56156e4fb57a3ae09db010cb4ca1c6344
Rakesh <rakeshfire97@gmail.com>
2017-11-29 23:28:30 +0530

    Create posoneg.cpp

---
+++ posoneg.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int i,j;
+cin>>i;
+if(i<0)
+{
+cout<<"Negative";
+}
+else
+{
+if(i==0)
+{
+cout<<"Zero";
+}
+else{
+cout<<"Positive";
+}
+}
+}
