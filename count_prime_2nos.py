5936585b0c3a6256dad178ceb861454e43775dfc
Rakesh <rakeshfire97@gmail.com>
2018-02-08 21:16:21 +0530

    Update and rename prime_from_a_b.py to count_prime_2nos.py

--- count_prime_2nos.py
+++ count_prime_2nos.py
@@ -1,10 +1,12 @@
 a1 = int(input())
 b1 = int(input())
 flag = 0
+count=0
 for x in range(a1,b1+1):
   for y in range(2,x):
     if x%y==0:
         flag =1
   if flag == 0:
-    print (x)
+    count+=1
   flag = 0
+print(count)  
